f9bbaaeb0a6aaeeefc42af08af3f0b280b4b723f
Kaleksanan Bagus Massani <curlyzed.lonely@gmail.com>
2026-06-08 12:22:08 +0700

    - delete junk files

--- CleanPad/Main.cpp
+++
@@ -1,6 +0,0 @@
-#include "CleanPad.h"
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
-    CleanPad app;
-    return app.Run(hInstance, nCmdShow);
-}
